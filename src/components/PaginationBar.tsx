e7fc9cabda79ce6c6d84c9b55acd8d8699af0b1d
James Hibbeard <jhibbeard@gmail.com>
2022-01-08 12:08:30 +0100

    Initial landing page moved over

---
+++ src/components/PaginationBar.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+import color from "./../assets/defaultColorPallet.json";
+
+const PaginationBar = ({ screen }: any) => {
+  const blobs = [1, 2, 3, 4];
+  return (
+    <View style={styles.pagination}>
+      {blobs.map((i, index) => {
+        const active =
+          index === screen
+            ? { width: 29, backgroundColor: color.Lavendar[50] }
+            : null;
+        return <View style={[styles.paginationBlob, active]} key={index} />;
+      })}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  pagination: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "center",
+  },
+  paginationBlob: {
+    width: 11,
+    height: 11,
+    borderRadius: 6,
+    backgroundColor: color.Lavendar[30],
+    marginHorizontal: 9,
+  },
+  active: {},
+});
+
+export default PaginationBar;
