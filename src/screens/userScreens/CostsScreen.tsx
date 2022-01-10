4dbe25dd4d18e58abed1950d197d5c719e576b20
James Hibbeard <jhibbeard@gmail.com>
2022-01-10 10:55:03 +0100

    Other pages added

---
+++ src/screens/userScreens/CostsScreen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, StyleSheet} from 'react-native';
+
+const CostsScreen = () => {
+  return <Text>Costs screen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default CostsScreen;
