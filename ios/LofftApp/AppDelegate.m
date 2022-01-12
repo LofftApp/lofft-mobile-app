573ce00d354c74f17a29419b9f2b61480e8a493a
James Hibbeard <jhibbeard@gmail.com>
2022-01-12 11:20:24 +0100

    FirebaseSetUp

--- ios/LofftApp/AppDelegate.m
+++ ios/LofftApp/AppDelegate.m
@@ -3,6 +3,7 @@
 #import <React/RCTBridge.h>
 #import <React/RCTBundleURLProvider.h>
 #import <React/RCTRootView.h>
+#import <Firebase.h>
 
 #import "RNBootSplash.h"
 
@@ -49,7 +50,7 @@ static void InitializeFlipper(UIApplication *application) {
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-  
+  [FIRApp configure];
   [RNBootSplash initWithStoryboard:@"BootSplash" rootView:rootView];
   
   return YES;
