2f59501449795650aebac1703ee36cdd10bb27e7
James Hibbeard <jhibbeard@gmail.com>
2022-02-05 15:15:47 +0100

    Storage rules are an issue, when they exist they block access to get and save data, but db rules are working fine

--- src/api/firebase/firebaseStorage.ts
+++ src/api/firebase/firebaseStorage.ts
@@ -3,25 +3,32 @@ import {utils} from '@react-native-firebase/app';
 import storage from '@react-native-firebase/storage';
 import {launchImageLibrary} from 'react-native-image-picker';
 import {uploadImageToUserProfile} from './fireStoreActions';
-import {getCurrentUser, addImageToAuth} from './firebaseApi';
+import {addImageToAuth} from './firebaseApi';
+import auth from '@react-native-firebase/auth';
 
 if (__DEV__) {
   storage().useEmulator('localhost', 9199);
 }
 
-export const userImageUpload = async docId => {
-  const currentUser = await getCurrentUser();
-  const result = await launchImageLibrary({mediaType: 'photo'});
-  const reference = storage().ref(`${currentUser.uid}/userImage/profile.jpg`);
-  const pathToFile = `${utils.FilePath.TEMP_DIRECTORY}/${result.assets[0].fileName}`;
-  // Upload file
-  await reference.putFile(pathToFile);
-  const url = await reference.getDownloadURL();
-  if (url) {
-    addImageToAuth(url);
-    uploadImageToUserProfile(docId, url);
+export const userImageUpload = docId => {
+  const response = url => {
     return url;
-  } else {
-    return {message: 'Action Cancelled'};
-  }
+  };
+  auth().onAuthStateChanged(async user => {
+    if (user) {
+      const result = await launchImageLibrary({mediaType: 'photo'});
+      const reference = storage().ref(`${user.uid}/userImage/profile.jpg`);
+      const pathToFile = `${utils.FilePath.TEMP_DIRECTORY}/${result.assets[0].fileName}`;
+      // Upload file
+      await reference.putFile(pathToFile);
+      const url = await reference.getDownloadURL();
+      if (url) {
+        addImageToAuth(url);
+        uploadImageToUserProfile(docId, url);
+        response(url);
+      } else {
+        return {message: 'Action Cancelled'};
+      }
+    }
+  });
 };
