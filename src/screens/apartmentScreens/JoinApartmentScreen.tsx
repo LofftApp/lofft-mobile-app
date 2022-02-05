f95eeba617e5454bb9389f88f6a57806bb1bd190
James Hibbeard <jhibbeard@gmail.com>
2022-02-05 19:54:11 +0100

    Search bar added to join page

--- src/screens/apartmentScreens/JoinApartmentScreen.tsx
+++ src/screens/apartmentScreens/JoinApartmentScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import {View, Text, StyleSheet, Platform} from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, StyleSheet, TextInput, Platform} from 'react-native';
 import {navigationRef as navigation} from '../../RootNavigation';
 
 // StyleSheets
@@ -9,8 +9,10 @@ import {fontStyles} from '../../StyleSheets/FontStyleSheet';
 
 // Componenets
 import CustomBackButton from '../../components/CustomBackButton';
+import {CoreButton} from '../../components/CoreButton';
 
 const JoinApartmentScreen = () => {
+  const [formInput, setFormInput] = useState('');
   return (
     <View
       style={[
@@ -21,10 +23,44 @@ const JoinApartmentScreen = () => {
         onPress={() => navigation.goBack()}
         title="Join a Lofft"
       />
+      <View style={styles.pageContainer}>
+        <Text style={fontStyles.bodyMedium}>
+          To join a Lofft, please enter the Loffts name of unique code below
+        </Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={[styles.formField, fontStyles.bodyMedium]}
+            placeholder="Name / Lofft ID"
+            autoCapitalize="none"
+            value={formInput}
+            onChangeText={e => setFormInput(e)}
+          />
+        </View>
+      </View>
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  pageContainer: {
+    flex: 1,
+    paddingVertical: 15,
+    marginVertical: 20,
+  },
+  inputContainer: {
+    backgroundColor: color.Lavendar[30],
+    height: 100,
+    borderRadius: 12,
+    paddingHorizontal: 10,
+    justifyContent: 'center',
+  },
+  formField: {
+    height: 40,
+    width: '100%',
+    backgroundColor: color.White[80],
+    borderRadius: 12,
+    paddingHorizontal: 15,
+  },
+});
 
 export default JoinApartmentScreen;
