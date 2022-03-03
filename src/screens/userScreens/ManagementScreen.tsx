b60ee61b4f70360a34bb9d2fb8eae1933bd2bed6
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2022-03-03 15:06:33 +0100

    function passing daea into parent added retriving the right date from calender

--- src/screens/userScreens/ManagementScreen.tsx
+++ src/screens/userScreens/ManagementScreen.tsx
@@ -1,6 +1,13 @@
-import React, { useState, useEffect, useCallback } from 'react';
-import { View, Text, StyleSheet, Platform, SafeAreaView, ScrollView } from 'react-native';
-import { List } from 'react-native-paper';
+import React, {useState, useEffect, useCallback} from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  ScrollView,
+} from 'react-native';
+import {List} from 'react-native-paper';
 
 // Components
 import HeaderBar from '../../components/bannersAndBars/HeaderBar';
@@ -8,21 +15,21 @@ import ToggleBar from './../../components/bannersAndBars/ToggleBar';
 import PollCard from '../../components/cards/PollCard';
 import NewPollContainer from '../../components/iconsAndContainers/NewPollContainer';
 
-
 // StyleSheets
-import { CoreStyleSheet } from '../../StyleSheets/CoreDesignStyleSheet';
-import { fontStyles } from '../../StyleSheets/FontStyleSheet';
+import {CoreStyleSheet} from '../../StyleSheets/CoreDesignStyleSheet';
+import {fontStyles} from '../../StyleSheets/FontStyleSheet';
+import CalendarManagement from '../../components/calendar/CalendarManagement';
+import {CoreButton} from '../../components/buttons/CoreButton';
 
-
-
-
-const ManagementScreen = ({ navigation, route }: any) => {
-
+const ManagementScreen = ({navigation, route}: any) => {
   // User Hooks
   const [image, setImage]: any = useState('');
   // Hooks
   const [pollsactivated, setPollsactivated] = useState(true);
-  const [expanded, setExpanded] = React.useState(true);
+  const [expanded, setExpanded] = useState(true);
+  const [date, setdate] = useState("")
+
+
 
   const buttonToggle = useCallback(toggled => {
     setPollsactivated(toggled);
@@ -30,93 +37,114 @@ const ManagementScreen = ({ navigation, route }: any) => {
 
   const handlePress = () => setExpanded(!expanded);
 
+  const fetchdate = (dateInput) => {
+    setdate(dateInput);
+  }
 
-  return(
+console.log(`Date is being fetched and assigned into setdate state: ${date}`)
 
+  return (
     <View
       style={[
         CoreStyleSheet.viewContainerStyle,
         Platform.OS === 'ios' ? CoreStyleSheet.viewContainerIOSStyle : null,
       ]}>
-
       <SafeAreaView style={styles.container}>
         <ScrollView
-        style={styles.scrollView}
-        showsVerticalScrollIndicator={false}
-        >
+          style={styles.scrollView}
+          showsVerticalScrollIndicator={false}>
+          <HeaderBar title="Management" image={image} />
 
-      <HeaderBar title="Management" image={image} />
-      <ToggleBar
-        optionA="Polls"
-        optionB="Flat's calendar"
-        dashboard={buttonToggle}
-      />
-
-      <NewPollContainer
-        buttonValue="Create New Poll"
-            buttonAction={() => navigation.navigate('MakeNewPoll')}
-      />
-
-      <List.Section>
-        <List.Accordion
-          title={<Text style={fontStyles.buttonTextLarge}>Active Polls</Text>}
-          style={styles.accordionContainer}
-          expanded={expanded}
-          onPress={handlePress}
-          >
-  {/* !!! ATTENTION POLLCARDS ARE HARD CODED THIS WHERE DB ITTERATION WILL TAKE PLACE !!! */}
-          <PollCard
-          value="Example"
-            buttonAction={() => navigation.navigate('')}
-          />
-          <PollCard
-            value="Example"
-            buttonAction={() => navigation.navigate('')}
-          />
-          <PollCard
-            value="Example"
-            buttonAction={() => navigation.navigate('')}
+          <ToggleBar
+            optionA="Polls"
+            optionB="Flat's calendar"
+            dashboard={buttonToggle}
           />
 
-        </List.Accordion>
-      </List.Section>
+          {pollsactivated ? (
+            <>
+              <NewPollContainer
+                buttonValue="Create New Poll"
+                buttonAction={() => navigation.navigate('MakeNewPoll')}
+              />
 
+              <List.Section>
+                <List.Accordion
+                  title={
+                    <Text style={fontStyles.buttonTextLarge}>Active Polls</Text>
+                  }
+                  style={styles.accordionContainer}
+                  expanded={expanded}
+                  onPress={handlePress}>
+                  {/* !!! ATTENTION POLLCARDS ARE HARD CODED THIS WHERE DB ITTERATION WILL TAKE PLACE !!! */}
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                </List.Accordion>
+              </List.Section>
 
-
-      <List.Section>
-        <List.Accordion
-          title={<Text style={fontStyles.buttonTextLarge}>Past Polls</Text>}
-          style={styles.accordionContainer}>
-          {/* !!! ATTENTION POLLCARDS ARE HARD CODED THIS WHERE DB ITTERATION WILL TAKE PLACE !!! */}
-          <PollCard
-            value="Example"
-            buttonAction={() => navigation.navigate('')}
-          />
-          <PollCard
-            value="Example"
-            buttonAction={() => navigation.navigate('')}
-          />
-          <PollCard
-            value="Example"
-            buttonAction={() => navigation.navigate('')}
-          />
-
-            </List.Accordion>
-          </List.Section>
+              <List.Section>
+                <List.Accordion
+                  title={
+                    <Text style={fontStyles.buttonTextLarge}>Past Polls</Text>
+                  }
+                  style={styles.accordionContainer}>
+                  {/* !!! ATTENTION POLLCARDS ARE HARD CODED THIS WHERE DB ITTERATION WILL TAKE PLACE !!! */}
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                  <PollCard
+                    value="Example"
+                    buttonAction={() => navigation.navigate('')}
+                  />
+                </List.Accordion>
+              </List.Section>
+            </>
+          ) : (
+            <>
+                <CalendarManagement fetchdate={fetchdate} />
+              <View style={styles.buttonContainer}>
+                <CoreButton
+                  value="Add new event"
+                  style={styles.button}
+                  invert
+                  onPress={() => navigation.navigate('')}
+                />
+              </View>
+            </>
+          )}
         </ScrollView>
       </SafeAreaView>
     </View>
-
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
-  accordionContainer:{
+  accordionContainer: {
     backgroundColor: 'white',
     display: 'flex',
     justifyContent: 'flex-start',
-
-  }
+  },
+  buttonContainer: {
+    marginVertical: 170,
+  },
+  button: {
+    marginVertical: 5,
+  },
 });
 
 export default ManagementScreen;
