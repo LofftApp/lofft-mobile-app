b46646a444b6895204eb4707b99a0b9e70a15bef
James Hibbeard <jhibbeard@gmail.com>
2022-03-07 14:11:10 +0100

    Copied in the design from the user profile

--- src/screens/apartmentScreens/ViewApartmentScreen.tsx
+++ src/screens/apartmentScreens/ViewApartmentScreen.tsx
@@ -3,10 +3,9 @@ import {
   View,
   Text,
   StyleSheet,
-  Platform,
-  TouchableOpacity,
   ImageBackground,
-  TextInput,
+  Image,
+  ScrollView,
   Alert,
 } from 'react-native';
 import {navigationRef as navigation} from '../../RootNavigation';
@@ -15,10 +14,7 @@ import shapesBackground from './../../assets/backgroundShapes/mint.png';
 
 // Firestore
 import firestore from '@react-native-firebase/firestore';
-import {
-  confirmUserLofft,
-  updateLofft,
-} from '../../api/firebase/fireStoreActions';
+import {confirmUserLofft} from '../../api/firebase/fireStoreActions';
 
 // stylesheets
 import color from '../../assets/defaultColorPallet.json';
@@ -27,17 +23,20 @@ import {fontStyles} from '../../StyleSheets/FontStyleSheet';
 
 // Components
 import CustomBackButton from '../../components/buttons/CustomBackButton';
-import UserIcon from '../../components/iconsAndContainers/UserIcon';
+// import UserIcon from '../../components/iconsAndContainers/UserIcon';
+import {navigationRef} from '../../RootNavigation';
+
+// Images
+import blueBackground from '../../assets/backgroundShapes/lavendar.png';
 
 const ViewApartmentScreen = ({route}) => {
   const [lofftId] = useState(route.params.lofft);
-  const [admin] = useState(true);
-  const [edit, setEdit] = useState(false);
   const [name, setName] = useState('');
   const [description, setDescription] = useState('');
   const [address, setAddress] = useState('');
   const [tenants, setTenants] = useState([]);
   const [update, setUpdate] = useState(false);
+  const [image, setImage]: any = useState({});
 
   const showAlert = (userId, lofftId) =>
     Alert.alert('Approve user', 'My Alert Msg', [
@@ -95,219 +94,162 @@ const ViewApartmentScreen = ({route}) => {
       });
   }, [update]);
   return (
-    <View
-      style={[
-        CoreStyleSheet.viewContainerStyle,
-        Platform.OS === 'ios' ? CoreStyleSheet.viewContainerIOSStyle : null,
-        styles.overidePresets,
-      ]}>
-      <View style={{paddingHorizontal: 25}}>
-        <CustomBackButton title="Lofft" onPress={() => navigation.goBack()} />
-      </View>
-      {/* Banner Background */}
-      <ImageBackground
-        source={shapesBackground}
-        style={styles.imageBannerBackground}>
-        <View style={styles.headerUpload}>
-          <Text>🏳️‍🌈 🌱</Text>
-          {admin ? (
-            <Icon name="image-outline" size={25} color={color.Black[30]} />
-          ) : null}
-        </View>
-        <View style={styles.headerBanner}>
-          {/* Title Text and address container */}
-          <View style={styles.title}>
-            <View>
-              {edit ? (
-                <TextInput
-                  value={name}
-                  style={[fontStyles.headerMedium, styles.inputField]}
-                  onChangeText={e => {
-                    setName(e);
-                  }}
-                />
-              ) : (
-                <Text style={fontStyles.headerMedium}>{name}</Text>
-              )}
-              {/* Address Section */}
-              {edit ? (
-                <View style={styles.addField}>
-                  <TextInput
-                    style={[fontStyles.bodySmall, styles.inputField]}
-                    value={address}
-                    onChangeText={e => setAddress(e)}
-                    placeholder="Address line 1"
-                  />
-                </View>
-              ) : address ? (
-                <View style={styles.addressBar}>
-                  <Text style={[fontStyles.bodySmall, styles.address]}>
-                    {address}
-                  </Text>
-                </View>
-              ) : null}
-            </View>
-            {/* Toggle Edit mode if admin */}
-            {admin ? (
-              edit ? (
-                <TouchableOpacity
-                  onPress={() => {
-                    updateLofft(lofftId, name, description, address);
-                    edit ? setEdit(false) : setEdit(true);
-                  }}>
-                  <Icon
-                    name="checkmark-outline"
-                    size={25}
-                    color={color.Black[30]}
-                  />
-                </TouchableOpacity>
-              ) : (
-                <TouchableOpacity
-                  onPress={() => {
-                    edit ? setEdit(false) : setEdit(true);
-                  }}>
-                  <Icon
-                    name="settings-outline"
-                    size={25}
-                    color={color.Black[30]}
-                  />
-                </TouchableOpacity>
-              )
-            ) : null}
-          </View>
+    <View style={styles.pageContainer}>
+      <ImageBackground source={blueBackground} style={styles.headerBackground}>
+        <CustomBackButton
+          style={styles.backButton}
+          neutral={true}
+          onPress={() => navigationRef.goBack()}
+        />
+        <View style={styles.imageHeaderContainer}>
+          <Text style={[fontStyles.headerMedium, styles.header]}>{name}</Text>
+          <Image source={image} style={styles.userImage} />
         </View>
       </ImageBackground>
-      <View style={styles.body}>
-        {/* Lofft Description Section */}
-        <View style={styles.desciptionContainer}>
-          {edit ? (
-            <TextInput
-              value={description}
-              style={[fontStyles.bodySmall, styles.inputField]}
-              multiline
-              onChangeText={e => {
-                setDescription(e);
-              }}
-            />
-          ) : (
-            <Text style={[fontStyles.bodySmall, styles.description]}>
-              {description}
-            </Text>
-          )}
+      <ScrollView style={CoreStyleSheet.viewContainerStyle}>
+        {/* <View style={styles.pillContainer}>
+          {tags.map(tag => {
+            return (
+              <TagIcon text={tag.value} key={tag.value} userColor={tag.color} />
+            );
+          })}
+        </View> */}
+        <Text style={[fontStyles.bodySmall, styles.userText]}>
+          {description}
+        </Text>
+        <Text style={fontStyles.buttonTextMedium}>Loffts</Text>
+        <View style={styles.noLofftContainer}>
+          <Text style={styles.noLofftText}>👀</Text>
+          <Text style={styles.noLofftText}>Nothing to see here</Text>
+          <Text style={styles.noLofftText}>They're a newbie</Text>
+          <Text style={styles.noLofftText}>...........</Text>
         </View>
-        {/* User/Tennants */}
-        <View style={styles.userWindow}>
-          <View style={styles.tenantSection}>
-            {tenants.map(t => {
-              return (
-                <View style={styles.userCard} key={t.name}>
-                  <UserIcon
-                    image={t.imageURI ? {uri: t.imageURI} : ''}
-                    onPress={() => showAlert(t.id, lofftId)}
-                    userIconStyle={styles.userIconStyle}
-                    userImageContainerStyle={styles.userImageContainerStyle}
-                    userImageStyle={styles.userImageStyle}
-                    disabled={t.pending ? false : true}
-                  />
-                  <Text
-                    style={[fontStyles.buttonTextMedium, styles.userCardText]}>
-                    {t.name.split(' ')[0]}
-                  </Text>
-                  {t.pending ? <Text>Pending</Text> : null}
-                </View>
-              );
-            })}
+        <Text style={fontStyles.buttonTextMedium}>Photo Library</Text>
+        <View style={styles.noLofftContainer}>
+          <View style={styles.addImageButton}>
+            <Icon name="add-outline" size={60} color={color.Black[30]} />
           </View>
         </View>
-      </View>
+        <Text style={fontStyles.buttonTextMedium}>Hobbies</Text>
+        <View style={styles.hobbyContaner}>
+          <View style={styles.hobby}>
+            <Icon name="megaphone-outline" size={36} color={color.Black[100]} />
+            <Text style={[fontStyles.bodySmall, styles.hobbyText]}>
+              Politics
+            </Text>
+          </View>
+          <View style={styles.hobby}>
+            <Icon
+              name="restaurant-outline"
+              size={36}
+              color={color.Black[100]}
+            />
+            <Text style={[fontStyles.bodySmall, styles.hobbyText]}>Cookng</Text>
+          </View>
+          <View style={styles.hobby}>
+            <Icon name="fast-food-outline" size={36} color={color.Black[100]} />
+            <Text style={[fontStyles.bodySmall, styles.hobbyText]}>
+              Eating Out
+            </Text>
+          </View>
+          <View style={styles.hobby}>
+            <Icon name="happy-outline" size={36} color={color.Black[100]} />
+            <Text style={[fontStyles.bodySmall, styles.hobbyText]}>
+              Meditation
+            </Text>
+          </View>
+          {/* Add Spotify / Apple Music API here */}
+        </View>
+      </ScrollView>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  overidePresets: {
-    paddingHorizontal: 0,
+  pageContainer: {
+    flex: 1,
   },
-  imageBannerBackground: {
-    backgroundColor: color.Mint[10],
-    paddingVertical: 10,
-    marginTop: 25,
-  },
-  headerBanner: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'flex-end',
-    height: 150,
-  },
-  headerUpload: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    paddingHorizontal: 25,
-  },
-  body: {
-    paddingHorizontal: 25,
-  },
-  textArea: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  headerBackground: {
     width: '100%',
-  },
-  title: {
-    width: '100%',
-    paddingHorizontal: 25,
-    justifyContent: 'space-between',
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  inputField: {
-    paddingTop: 0,
-    paddingBottom: 0,
-    paddingVertical: 10,
+    height: 200,
     backgroundColor: color.Lavendar[10],
-    marginTop: 0,
   },
-  desciptionContainer: {
-    marginTop: 25,
+  backButton: {
+    marginHorizontal: 15,
+    marginTop: 35,
   },
-  addField: {
+  pillContainer: {
+    flexDirection: 'row',
+  },
+  imageHeaderContainer: {
     flexDirection: 'row',
     justifyContent: 'space-between',
+    flex: 1,
+    alignItems: 'flex-end',
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+  },
+  header: {
+    maxWidth: '60%',
+  },
+  userImage: {
+    width: 90,
+    height: 90,
+    borderWidth: 4,
+    borderColor: color.Lavendar[100],
+    borderRadius: 75,
+  },
+  pill: {
+    width: 78,
+    height: 19,
+    borderWidth: 1,
+    borderColor: color.Lavendar[100],
+    borderRadius: 6,
+    backgroundColor: color.Lavendar[5],
+    justifyContent: 'center',
     alignItems: 'center',
-    marginTop: 15,
+    marginRight: 15,
   },
-  address: {},
-  addressBar: {
-    paddingLeft: 10,
+  fontColor: {
+    color: color.Lavendar[100],
   },
-  description: {
+  userText: {
     textAlign: 'justify',
+    color: color.Black[50],
+    marginVertical: 15,
   },
-  userWindow: {
-    marginTop: 15,
-  },
-  tenantSection: {
+  noLofftContainer: {
     width: '100%',
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginTop: 15,
-  },
-  userCard: {
     alignItems: 'center',
-    marginHorizontal: 10,
+    paddingVertical: 15,
   },
-  userCardText: {marginVertical: 10},
-  userIconStyle: {
-    width: 85,
-    height: 85,
+  noLofftText: {
+    color: color.Black[50],
+    marginVertical: 3,
   },
-  userImageContainerStyle: {
-    width: 70,
-    height: 70,
-    borderColor: color.White[0],
+  addImageButton: {
+    width: 95,
+    height: 95,
+    backgroundColor: color.Black[10],
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 8,
+    alignSelf: 'flex-start',
   },
-  userImageStyle: {
-    width: 70,
-    height: 70,
+  hobbyContaner: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+  },
+  hobby: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginVertical: 10,
+    flexBasis: '50%',
+  },
+  hobbyText: {
+    marginHorizontal: 20,
   },
 });
 
