6b7d95c072659e29e223adc3b2c7e5b6fc397fc8
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2022-03-16 13:58:00 +0100

    calendar selected dates are now marking on click

--- src/components/calendar/CalendarManagement.tsx
+++ src/components/calendar/CalendarManagement.tsx
@@ -1,6 +1,6 @@
-import React from "react";
-import { View, Text, StyleSheet} from 'react-native';
-import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
 
 const headerFont700 = 'Montserrat-Bold';
 const headerFont600 = 'Montserrat-SemiBold';
@@ -9,14 +9,23 @@ const bodyFont = 'Roboto';
 
 // StyleSheets
 import color from './../../assets/defaultColorPallet.json';
-import { CoreStyleSheet } from '../../StyleSheets/CoreDesignStyleSheet';
-import { fontStyles } from './../../StyleSheets/FontStyleSheet';
-import { useState } from "react";
+import {CoreStyleSheet} from '../../StyleSheets/CoreDesignStyleSheet';
+import {fontStyles} from './../../StyleSheets/FontStyleSheet';
+import {useState} from 'react';
 
 const CalendarManagement = ({fetchdate}) => {
+  const [date, setdate] = useState({});
 
+  const getSelectedDayEvents = date => {
+    let markedDate = {};
+    markedDate[date] = {selected: true};
+    console.log(`Hello from within the function ${markedDate}`);
+    setdate(markedDate);
+  };
 
-  return(
+  console.log(`Hello from state ${date}`);
+
+  return (
     <Calendar
       disableMonthChange={true}
       theme={{
@@ -24,7 +33,7 @@ const CalendarManagement = ({fetchdate}) => {
         calendarBackground: '#ffffff',
         textSectionTitleColor: 'black',
         textSectionTitleDisabledColor: '#d9e1e8',
-        selectedDayBackgroundColor: '#00adf5',
+        selectedDayBackgroundColor: '#87E5C7',
         selectedDayTextColor: '#ffffff',
         todayTextColor: '#724EFA',
         dayTextColor: '#2d4150',
@@ -43,29 +52,23 @@ const CalendarManagement = ({fetchdate}) => {
         textDayHeaderFontWeight: 'bold',
         textDayFontSize: 16,
         textMonthFontSize: 16,
-        textDayHeaderFontSize: 14
+        textDayHeaderFontSize: 14,
       }}
       style={styles.calmar}
-
       onDayPress={day => {
+        getSelectedDayEvents(day.dateString);
         console.log('selected day', day.dateString);
-        fetchdate(day.dateString)
+        fetchdate(day.dateString);
       }}
-
-      markedDates={{
-        '2022-03-16': { selected: true, marked: true, selectedColor: '#724EFA' },
-        // This is Probably where we are going to read in the dates from db to mark!
-      }}
+      markedDates={date}
     />
-
-
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   calmar: {
     marginTop: 15,
-  }
+  },
 });
 
 export default CalendarManagement;
