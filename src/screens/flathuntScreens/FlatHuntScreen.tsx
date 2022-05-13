6c77787bc75650aacbf5fe3b1f27e18a7b6f555d
Adam Tomczyk <markus.adam.tomczyk@gmail.com>
2022-05-13 13:07:32 +0200

    created a new component for the flathunt header that holds an image and a back button

--- src/screens/flathuntScreens/FlatHuntScreen.tsx
+++ src/screens/flathuntScreens/FlatHuntScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
 
 import {
   View,
@@ -10,75 +10,52 @@ import {
   ScrollView,
 } from 'react-native';
 
-
 // Components 🪢
 import HeaderBar from '../../components/bannersAndBars/HeaderBar';
 import CustomBackButton from '../../components/buttons/CustomBackButton';
-import { CoreButton } from '../../components/buttons/CoreButton';
+import {CoreButton} from '../../components/buttons/CoreButton';
 
 // Stylesheets
 import color from './../../assets/defaultColorPallet.json';
-import { CoreStyleSheet } from '../../StyleSheets/CoreDesignStyleSheet';
-import { fontStyles } from './../../StyleSheets/FontStyleSheet';
-import { navigationRef } from '../../RootNavigation';
+import {CoreStyleSheet} from '../../StyleSheets/CoreDesignStyleSheet';
+import {fontStyles} from './../../StyleSheets/FontStyleSheet';
+import {navigationRef as navigation} from './../../RootNavigation';
+import HeaderBarFlatHunt from '../../components/bannersAndBars/HeaderBarFlatHunt';
 
 const FlatHuntScreen = () => {
-
   const [image, setImage]: any = useState('');
   const [city, setCity]: any = useState('');
 
-  return(
+  return (
     <View
       style={[
         CoreStyleSheet.viewContainerStyle,
         Platform.OS === 'ios' ? CoreStyleSheet.viewContainerIOSStyle : null,
       ]}>
-      <SafeAreaView style={styles.container}>
+      <SafeAreaView>
         <ScrollView
           style={styles.scrollView}
           showsVerticalScrollIndicator={false}>
-          <View style={styles.HeaderBarFlex}>
-            <Text style={[fontStyles.headerMedium]}>Looking for a new <Text style={{color:color.Lavendar[80]}}>Home</Text>?</Text>
-            <HeaderBar title="" image={image} />
-          </View>
-
-          <View style={{marginTop: 20}}>
-            <Text style={fontStyles.buttonTextMedium}>Where? 👀</Text>
-            <View style={styles.questionInputContainer}>
-              <TextInput
-                style={[styles.questionInputStyle, fontStyles.bodyMedium]}
-                keyboardType="email-address"
-                placeholder="Berlin for Instance?"
-                autoCapitalize="none"
-                value={city}
-                onChangeText={text => setCity(text)}
-                multiline={false}
-              />
-            </View>
-          </View>
-
-        </ScrollView>
-        </SafeAreaView>
+          <HeaderBarFlatHunt onPress={() => navigation.goBack()} />
+        <View style={styles.container}>
+            <Text style={fontStyles.headerMedium}>Looking for a new Home?</Text>
         </View>
+        </ScrollView>
+      </SafeAreaView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
-  HeaderBarFlex: {
-    flexDirection: 'row',
-    width: '49%',
-    alignItems: 'flex-start',
-  },
-  questionInputStyle: {
-    height: 56,
+  container: {
+    display: 'flex',
+    alignItems: 'center',
     width: '100%',
-    paddingHorizontal: 15,
-    fontSize: 24,
-    marginVertical: 5,
-    backgroundColor: color.Lavendar[10],
-    color: color.Black[80],
-    borderRadius: 16,
   },
+  headerBar: {
+    display: 'flex',
+    flexDirection: 'row'
+  }
 });
 
 export default FlatHuntScreen;
