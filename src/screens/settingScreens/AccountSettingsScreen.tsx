de229aa89a52d9bbd6c07c579a76cbfd4ec90f27
Donghwan Kim <donghwan.kim92@gmail.com>
2022-07-01 15:38:50 +0200

    added page layout

--- src/screens/settingScreens/AccountSettingsScreen.tsx
+++ src/screens/settingScreens/AccountSettingsScreen.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {Text, View, StyleSheet, Platform} from 'react-native';
+
+// Components
+import HeaderBar from '../../components/bannersAndBars/HeaderBar';
+
+// Style Sheets
+import {CoreStyleSheet} from '../../StyleSheets/CoreDesignStyleSheet';
+import {fontStyles} from '../../StyleSheets/FontStyleSheet';
+import color from '../../assets/defaultColorPallet.json';
 
 const AccountSettingsScreen = () => {
   return (
-    <View>
+    <View
+      style={[
+        CoreStyleSheet.viewContainerStyle,
+        Platform.OS === 'ios' ? CoreStyleSheet.viewContainerIOSStyle : null,
+      ]}>
+      <HeaderBar title="My Settings" />
       <Text>account screen</Text>
     </View>
   );
